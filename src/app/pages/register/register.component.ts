e69837d402d6a89ffe2692096bf7ec0149046b25
Franklin <franklinpereira98@hotmail.com>
2025-03-19 16:29:42 -0300

    Creating register forms

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FooterComponent, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
